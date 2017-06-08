2a1e523bc747fdca4b73a012a6a0d3bdb3261001
Manabu Shibata <None>
2017-06-08 17:55:31 +0900

    add code to train_routes

--- app/models/train_route.rb
+++ app/models/train_route.rb
@@ -1,3 +1,5 @@
 class TrainRoute < ActiveRecord::Base
   has_and_belongs_to_many :stations
+
+  validates :code, uniqueness: true
 end
--- db/migrate/20170607053817_create_train_routes.rb
+++ db/migrate/20170607053817_create_train_routes.rb
@@ -1,9 +1,11 @@
 class CreateTrainRoutes < ActiveRecord::Migration
   def change
     create_table :train_routes do |t|
+      t.integer :code
       t.string :name
 
       t.timestamps null: false
     end
+    add_index :train_routes, :code, unique: true
   end
 end
