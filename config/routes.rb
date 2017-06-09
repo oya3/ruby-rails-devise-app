7dd127e25006d2f89fccc69f43ad3a9173c159e8
Kazunori OYA <kazunori_oya@nsware.co.jp>
2017-06-09 11:08:02 +0900

    add only devise/registrations edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :train_routes
   resources :stations
-  # devise_for :users
   devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   namespace "admin" do
     resources :users
   end
