e50ae6deed11f68d971359549e495141a5f6316a
Kazunori OYA <terje@oya3.net>
2017-06-18 13:45:22 +0900

    fix TrainRouteStations belong_to TrainRoutes, make the ranker scoped to one train_route

--- app/models/train_route_station.rb
+++ app/models/train_route_station.rb
@@ -1,9 +1,10 @@
 # coding: utf-8
 class TrainRouteStation < ActiveRecord::Base
-  include RankedModel
-  ranks :row_order
   belongs_to :train_route
   belongs_to :station
+  include RankedModel
+  ranks :row_order,
+        :with_same => :train_route_id
 
   # validates :distance, presence: true # nil許可するため
   # validates :row_order, presence: true, numericality: true
